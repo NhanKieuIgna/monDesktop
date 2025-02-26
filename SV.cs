80c9cd7d2edd575ded1972112abf40da98ddccf7
NhanKieuIgna <nhankieuquang@gmail.com>
2025-02-26 10:26:55 +0700

    Add files via upload

---
+++ SV.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTH1
+{
+    internal abstract class SV
+    {
+        protected string maSV;
+        protected string hoTen;
+        protected string namSinh;
+
+        public SV(string maSV, string hoTen, string namSinh)
+        {
+            MaSV = maSV;
+            this.HoTen = hoTen;
+            this.NamSinh = namSinh;
+        }
+
+        public SV() { }
+        public string MaSV { get => maSV; set => maSV = value; }
+        public string HoTen { get => hoTen; set => hoTen = value; }
+        public string NamSinh { get => namSinh; set => namSinh = value; }
+
+        public abstract double tinhDiemTB();
+
+        public override string ToString()
+        {
+            return maSV + ", " + hoTen + ", " + namSinh;
+        }
+    }
+}
---
+++ SVCNTT.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTH1
+{
+    internal class SVCNTT : SV
+    {
+        private double diemPascal;
+        private double diemCSharp;
+        private double diemSQL;
+
+        public SVCNTT(string maSV, string hoTen, string namSinh, double diemPascal, double diemCSharp, double diemSQL) : base(maSV, hoTen, namSinh)
+        {
+            DiemPascal = diemPascal;
+            DiemCSharp = diemCSharp;
+            DiemSQL = diemSQL;
+        }
+
+         
+
+        public double DiemCSharp { get => diemCSharp; set => diemCSharp = value; }
+        public double DiemPascal { get => diemPascal; set => diemPascal = value; }
+        public double DiemSQL { get => diemSQL; set => diemSQL = value; }
+
+        public override double tinhDiemTB()
+        {
+            return (diemCSharp + diemPascal + diemSQL) / 3;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ", Diem Pascal: " + diemPascal +  ", diem C#: " + diemCSharp + ", diem SQL: " + diemSQL + ", Trung binh: " + tinhDiemTB();
+        }
+    }
+}
